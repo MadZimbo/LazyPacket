4c8423d41b314580d81944b25c5040d347795ef7
MadZimbo <47391785+MadZimbo@users.noreply.github.com>
2026-02-17 12:34:16 +0100

    appicon change

--- LazyPacket/SceneDelegate.swift
+++
@@ -1,3 +0,0 @@
-// DELETED: UIKit SceneDelegate
-// This file has been removed - SwiftUI App lifecycle is used instead  
-// See LazyPacketApp.swift for the modern SwiftUI app structure
--- LazyPacket/ViewController.swift
+++
@@ -1,4 +0,0 @@
-// DELETED: Legacy AppKit/NSViewController code
-// This file has been removed as part of migration to SwiftUI  
-// All functionality moved to WakeOnLANViewModel.swift and SwiftUI views
-
